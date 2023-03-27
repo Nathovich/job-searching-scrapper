11d85f20efebc6d759a792b045c9e80fbe2dd2f8
Nathovich <nathovich@gmail.com>
2023-03-27 16:42:15 +0200

    beta V.1.1

---
+++ urls.py
@@ -0,0 +1,5 @@
+pracuj_urls = [
+    "https://www.pracuj.pl/praca/ma%C5%82opolskie;r,6/ostatnich%207%20dni;p,7?pn={no_page}&tc=0&ws=0&wm=full-office%2Chybrid%2Chome-office",
+    "https://www.pracuj.pl/praca/mazowieckie;r,7/ostatnich%207%20dni;p,7?et=1%2C3%2C17%2C4&pn={no_page}&tc=0&ws=0&wm=hybrid%2Chome-office",
+    "https://www.pracuj.pl/praca/wroclaw;wp/ostatnich%207%20dni;p,7/praca%20zdalna;wm,home-office?rd=0&et=1%2C3%2C17%2C4&pn={no_page}&tc=0&ws=0",
+]
